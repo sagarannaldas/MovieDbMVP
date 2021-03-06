51d061c286b51c1047360ed352bcea2de952df6d
sagar <annal7057@gmail.com>
2021-03-06 16:19:58 +0530

    data model object for json request response

---
+++ app/src/main/java/in/techrebounce/moviedbmvp/model/MovieListResponse.kt
@@ -0,0 +1,31 @@
+package `in`.techrebounce.moviedbmvp.model
+
+data class MovieListResponse(
+    val dates: Dates,
+    val page: Int,
+    val results: List<Result>,
+    val total_pages: Int,
+    val total_results: Int
+) {
+    data class Dates(
+        val maximum: String,
+        val minimum: String
+    )
+
+    data class Result(
+        val adult: Boolean,
+        val backdrop_path: Any,
+        val genre_ids: List<Int>,
+        val id: Int,
+        val original_language: String,
+        val original_title: String,
+        val overview: String,
+        val popularity: Double,
+        val poster_path: String,
+        val release_date: String,
+        val title: String,
+        val video: Boolean,
+        val vote_average: Int,
+        val vote_count: Int
+    )
+}
