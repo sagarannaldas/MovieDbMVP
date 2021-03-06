851cfa617fd33b63ba5f73d73e19eb80362f487c
sagar <annal7057@gmail.com>
2021-03-06 18:40:18 +0530

    added model,view,presenter interface in contract interface

---
+++ app/src/main/java/in/techrebounce/moviedbmvp/contract/MovieListContract.kt
@@ -0,0 +1,28 @@
+package `in`.techrebounce.moviedbmvp.contract
+
+import `in`.techrebounce.moviedbmvp.model.Movie
+
+interface MovieListContract {
+
+    interface Model {
+        interface OnFinishedListener {
+            fun onFinished(movieArrayList: List<Movie>)
+            fun onFailure(t: Throwable)
+        }
+
+        fun getMovieList(onFinishedListener: OnFinishedListener, pageNo: Int)
+    }
+
+    interface View {
+        fun showProgress()
+        fun hideProgress()
+        fun setDataToRecyclerView(movieArrayList: List<Movie>)
+        fun onResponseFailure(t: Throwable)
+    }
+
+    interface Presenter {
+        fun onDestroy()
+        fun getMoreData(pageNo: Int)
+        fun requestDataFromServer()
+    }
+}
