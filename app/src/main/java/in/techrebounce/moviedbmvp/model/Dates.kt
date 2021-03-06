67d27bc03b6c32c6ca45ca461f2cab18505bda3c
sagar <annal7057@gmail.com>
2021-03-06 16:24:46 +0530

    created 3 data model object for One json request response

---
+++ app/src/main/java/in/techrebounce/moviedbmvp/model/Dates.kt
@@ -0,0 +1,6 @@
+package in.techrebounce.moviedbmvp.model
+
+data class Dates(
+    val maximum: String,
+    val minimum: String
+)
--- app/src/main/java/in/techrebounce/moviedbmvp/model/MovieListResponse.kt
+++ app/src/main/java/in/techrebounce/moviedbmvp/model/MovieListResponse.kt
@@ -1,4 +1,4 @@
-package `in`.techrebounce.moviedbmvp.model
+package in.techrebounce.moviedbmvp.model
 
 data class MovieListResponse(
     val dates: Dates,
@@ -6,26 +6,4 @@ data class MovieListResponse(
     val results: List<Result>,
     val total_pages: Int,
     val total_results: Int
-) {
-    data class Dates(
-        val maximum: String,
-        val minimum: String
-    )
-
-    data class Result(
-        val adult: Boolean,
-        val backdrop_path: Any,
-        val genre_ids: List<Int>,
-        val id: Int,
-        val original_language: String,
-        val original_title: String,
-        val overview: String,
-        val popularity: Double,
-        val poster_path: String,
-        val release_date: String,
-        val title: String,
-        val video: Boolean,
-        val vote_average: Int,
-        val vote_count: Int
-    )
-}
+)
