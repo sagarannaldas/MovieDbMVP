94aa73c8ea685290bec5228845eb38348ce6d7e2
sagar <annal7057@gmail.com>
2021-03-07 18:12:40 +0530

    Room movie table

--- app/src/main/java/in/techrebounce/moviedbmvp/model/Movie.kt
+++ app/src/main/java/in/techrebounce/moviedbmvp/model/Movie.kt
@@ -1,18 +1,39 @@
 package `in`.techrebounce.moviedbmvp.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "movie")
 data class Movie(
+    @PrimaryKey(autoGenerate = true)
+    var movie_id: Int? = null,
+    @ColumnInfo(name = "adult")
     val adult: Boolean,
+    @ColumnInfo(name = "backdrop_path")
     val backdrop_path: Any,
+    @ColumnInfo(name = "genre_ids")
     val genre_ids: List<Int>,
+    @ColumnInfo(name = "id")
     val id: Int,
+    @ColumnInfo(name = "original_language")
     val original_language: String,
+    @ColumnInfo(name = "original_title")
     val original_title: String,
+    @ColumnInfo(name = "overview")
     val overview: String,
+    @ColumnInfo(name = "popularity")
     val popularity: Double,
+    @ColumnInfo(name = "poster_path")
     val poster_path: String,
+    @ColumnInfo(name = "release_date")
     val release_date: String,
+    @ColumnInfo(name = "title")
     val title: String,
+    @ColumnInfo(name = "video")
     val video: Boolean,
+    @ColumnInfo(name = "vote_average")
     val vote_average: Double,
+    @ColumnInfo(name = "vote_count")
     val vote_count: Int
 )
